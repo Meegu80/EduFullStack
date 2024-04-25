c66ae12ad8c814a3c233fafb0a4b6663a81f3efa
Meegu80 <aceman80@nate.com>
2024-04-25 14:12:34 +0900

    Initial commit

---
+++ src/com/javalab/sec03/Audio.java
@@ -0,0 +1,20 @@
+package com.javalab.sec02;
+
+public class Audio {
+    private String model;
+
+    public Audio() {
+    }
+
+    public Audio(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
